0421238513a06a36d803570d716ddcdde12ab05b
Harsh07-X <harshsinghx707@gmail.com>
2026-01-14 21:44:46 +0530

    Started learning about List and different methods in List

---
+++ Day-3.part2/list.methods.py
@@ -0,0 +1,13 @@
+friends=["apple","orange","akash",5,False]
+
+print(friends)
+
+friends.append("banana")
+
+print(friends)
+
+l1=[1,2,4,5,8,89,4,3,2]
+# l1.sort()
+# l1.reverse()
+l1.insert(4,56)
+print(l1)
---
+++ Day-3.part2/list.py
@@ -0,0 +1,6 @@
+friends=["apple","orange","akash",5,False]
+
+# print(friends[0])
+friends[0]="grapes"
+print(friends[0])
+print(friends[0:4])
